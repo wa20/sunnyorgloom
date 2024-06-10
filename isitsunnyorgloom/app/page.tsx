63eca7a60546fe12ff54659e4dc603077cf9468b
wael <78425538+wa20@users.noreply.github.com>
2024-06-10 14:15:10 +0100

    imported renderWeather component

--- isitsunnyorgloom/app/page.tsx
+++ isitsunnyorgloom/app/page.tsx
@@ -1,9 +1,36 @@
+import RenderWeather from "@/components/RenderWeather";
+import { Button } from "@/components/ui/button";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+
+  const { userId } = auth();
+  
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-around p-24"> 
+      <RenderWeather />
       <h1>hello</h1>
+      
+
+      {/* {userId ? (
+        <Link
+          href="/translate"
+          className="bg-blue-500 hover:bg-blue-600 w-full mt-10 lg:w-fit p-5 rounded-md text-white text-center cursor-pointer hover:shadow-xl active:scale-90 duration-150 ease-out"
+        >
+          Get Mish&apos;in or dye Gloom&apos;in!
+        </Link>
+      ) : (
+        <Button className="bg-blue-500 hover:bg-blue-600 w-full mt-10 lg:w-fit p-5 hover:shadow-xl active:scale-90 duration-150 ease-out">
+          <SignInButton forceRedirectUrl="/translate" mode="modal">
+           Sunny | Gloom | Stream
+          </SignInButton>
+        </Button>
+      )} */}
     </main>
   );
 }
