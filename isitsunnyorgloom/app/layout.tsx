ab5bed3aed7d0d9eb55473a02ed41baea6e4b2e3
wael <78425538+wa20@users.noreply.github.com>
2024-06-10 14:48:29 +0100

    created a theme provider and added iported it to layout file

--- isitsunnyorgloom/app/layout.tsx
+++ isitsunnyorgloom/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
           <Header />
           <div className="max-w-6xl mx-auto">{children}</div>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
