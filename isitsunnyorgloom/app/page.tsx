e980a94a89f0665e8dfeee76cbb8ce173ceae880
wael <78425538+wa20@users.noreply.github.com>
2024-06-10 15:18:38 +0100

    added customisation to page file

--- isitsunnyorgloom/app/page.tsx
+++ isitsunnyorgloom/app/page.tsx
@@ -1,20 +1,34 @@
 import RenderWeather from "@/components/RenderWeather";
+import { currentUser } from '@clerk/nextjs/server';
 import { Button } from "@/components/ui/button";
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
-
+export default async function Home() {
+  const user = await currentUser();
   const { userId } = auth();
+
+
+  const firstName = user?.firstName
+  const imageUrl = user?.imageUrl
+
+  console.log("user", user);
+
   
 
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-24"> 
       <RenderWeather />
-      <h1>hello</h1>
+
+     { user ? (
+      <h1>Hello: {user?.firstName}</h1>
+     ) 
+     : 
+     (<h1>Hello</h1>)}
+      
       
 
       {/* {userId ? (
