f25eccdb9f4db3fe8aae2f40185b4e88af4a39f5
wael <78425538+wa20@users.noreply.github.com>
2024-06-11 14:10:04 +0100

    avatar component added and added colour variants in config file

--- isitsunnyorgloom/next.config.mjs
+++ isitsunnyorgloom/next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'openweathermap.org',
+          },
+          {
+            protocol: 'https',
+            hostname: 'links.papareact.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'limg.clerk.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'getlinked.blob.core.windows.net',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
